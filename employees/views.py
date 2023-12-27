72f813a6198c92f06fcdd52f3e8b5b9e075f1479
unknown <victorsimasdev@gmail.com>
2023-12-27 14:20:04 -0300

    altered homepage, added employee list, employee card page

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from employees.models import Employee
 
-# Create your views here.
+def employee_detail(request, pk):
+    employee = get_object_or_404(Employee, pk=pk)
+    context = {
+        'employee': employee
+    }
+    return render(request, 'employee_detail.html', context)
